09244f7ca91c5523d2cce8ff32620537f008e88c
ADMIN <saylinail277@gmail.com>
2026-06-26 22:04:45 +0530

    Initial commit

---
+++ src/Salary1/Demo.java
@@ -0,0 +1,29 @@
+package Salary1;
+
+
+
+public class Demo {
+	
+	public static void mian(String[] args) {
+		
+//		double salary = 40000.00;
+//		double bonus = 2000;
+//		double tax = 1000;
+//		double pf = 2500;
+//		double Total = salary + bonus - tax - pf;
+//		System.out.println("FinalSalary :"+ Total);
+		
+//		
+//		//EMI Calculation
+//		double loanAmount = 120000.00;
+//		int month = 12;
+//		double emi = loanAmount / month;
+//		System.out.println("MonthlyEMI :"+ emi);
+//		
+		
+		
+		
+		
+	}
+
+}
