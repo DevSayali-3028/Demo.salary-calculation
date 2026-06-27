2420582ea2ab818f252785ef3b808e780232c033
ADMIN <saylinail277@gmail.com>
2026-06-27 11:46:31 +0530

    Updated Salary1 Demo program

--- src/Salary1/Demo.java
+++ src/Salary1/Demo.java
@@ -6,20 +6,20 @@ public class Demo {
 	
 	public static void mian(String[] args) {
 		
-//		double salary = 40000.00;
-//		double bonus = 2000;
-//		double tax = 1000;
-//		double pf = 2500;
-//		double Total = salary + bonus - tax - pf;
-//		System.out.println("FinalSalary :"+ Total);
+	double salary = 40000.00;
+		double bonus = 2000;
+		double tax = 1000;
+		double pf = 2500;
+		double Total = salary + bonus - tax - pf;
+		System.out.println("FinalSalary :"+ Total);
 		
-//		
-//		//EMI Calculation
-//		double loanAmount = 120000.00;
-//		int month = 12;
-//		double emi = loanAmount / month;
-//		System.out.println("MonthlyEMI :"+ emi);
-//		
+		
+		//EMI Calculation
+		double loanAmount = 120000.00;
+		int month = 12;
+		double emi = loanAmount / month;
+	System.out.println("MonthlyEMI :"+ emi);
+		
 		
 		
 		
